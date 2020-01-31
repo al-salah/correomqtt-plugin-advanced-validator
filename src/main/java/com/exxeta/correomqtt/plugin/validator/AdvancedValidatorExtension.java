fa60c7a743ad0dbfcf28a001d1b2994759851069
Reinhold, Frieder <frieder.reinhold@exxeta.com>
2020-01-31 09:51:43 +0100

    initial commit

---
+++ src/main/java/com/exxeta/correomqtt/plugin/validator/AdvancedValidatorExtension.java
@@ -0,0 +1,28 @@
+package com.exxeta.correomqtt.plugin.validator;
+
+import com.exxeta.correomqtt.plugin.manager.PluginSystem;
+import com.exxeta.correomqtt.plugin.spi.MessageValidatorHook;
+import org.jdom2.Element;
+import org.pf4j.Extension;
+
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+@Extension
+public class AdvancedValidatorExtension implements MessageValidatorHook {
+
+    private ArrayList<MessageValidatorHook> validators;
+
+    @Override
+    public void onConfigReceived(Element config) {
+        // TODO come up with a nice protocol structure to combine different validators to support OR and AND algebra
+        this.validators = PluginSystem.getInstance().getExtensions(MessageValidatorHook.class, config).stream()
+                .filter(v -> !v.getClass().getSimpleName().equals(this.getClass().getSimpleName()))
+                .collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    @Override
+    public Validation isMessageValid(String message) {
+        return new Validation(validators.stream().allMatch(v -> v.isMessageValid(message).isValid()), "All valid");
+    }
+}
