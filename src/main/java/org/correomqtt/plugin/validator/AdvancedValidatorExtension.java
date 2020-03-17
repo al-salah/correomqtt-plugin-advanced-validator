9db46721b63f006e521fb6d4e1cd9280c1d3d172
Jonas Rau <rau.jonas@web.de>
2020-03-17 20:05:28 +0100

    Change package name

--- src/main/java/org/correomqtt/plugin/validator/AdvancedValidatorExtension.java
+++ src/main/java/org/correomqtt/plugin/validator/AdvancedValidatorExtension.java
@@ -1,7 +1,7 @@
-package com.exxeta.correomqtt.plugin.validator;
+package org.correomqtt.plugin.validator;
 
-import com.exxeta.correomqtt.plugin.manager.PluginSystem;
-import com.exxeta.correomqtt.plugin.spi.MessageValidatorHook;
+import org.correomqtt.plugin.manager.PluginSystem;
+import org.correomqtt.plugin.spi.MessageValidatorHook;
 import org.jdom2.Element;
 import org.pf4j.Extension;
 
